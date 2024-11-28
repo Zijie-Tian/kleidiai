5b3de06bf061e83e4212976877796a70c2ca52a7
Emil Ohlsson <emil.ohlsson@arm.com>
2024-11-28 13:11:42 +0000

    Fix copy/paste error in kernel interfaces
    This change fixes a minor copy paste error in the kernel interfaces.
    
    Signed-off-by: Emil Ohlsson <emil.ohlsson@arm.com>
    
    Approved-by: Jakub Sujak <jakub.sujak@arm.com>

--- kai/ukernels/matmul/matmul_clamp_f32_qsi8d32p_qsi4c32p/kai_matmul_clamp_f32_qsi8d32p_qsi4c32p_interface.h
+++ kai/ukernels/matmul/matmul_clamp_f32_qsi8d32p_qsi4c32p/kai_matmul_clamp_f32_qsi8d32p_qsi4c32p_interface.h
@@ -41,7 +41,7 @@ struct kai_matmul_clamp_f32_qsi8d32p_qsi4c32p_ukernel {
     kai_matmul_clamp_f32_qsi8d32p_qsi4c32p_get_n_step_func_t get_n_step;
     kai_matmul_clamp_f32_qsi8d32p_qsi4c32p_get_mr_func_t get_mr;
     kai_matmul_clamp_f32_qsi8d32p_qsi4c32p_get_nr_func_t get_nr;
-    kai_matmul_clamp_f32_qsi8d32p_qsi4c32p_get_nr_func_t get_kr;
+    kai_matmul_clamp_f32_qsi8d32p_qsi4c32p_get_kr_func_t get_kr;
     kai_matmul_clamp_f32_qsi8d32p_qsi4c32p_get_sr_func_t get_sr;
     kai_matmul_clamp_f32_qsi8d32p_qsi4c32p_get_lhs_packed_offset_func_t get_lhs_packed_offset;
     kai_matmul_clamp_f32_qsi8d32p_qsi4c32p_get_rhs_packed_offset_func_t get_rhs_packed_offset;
